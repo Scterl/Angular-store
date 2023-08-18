b3329cc18ac233ac9995d8b6144dc79aaac25e60
Administrator <admin@example.com>
2023-08-18 13:49:54 +0800

    feat: 导航和添加

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core';
+import {Ingredient} from "../../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
+   @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  // {类型的定义}
+  // @Output() ingredientAdded = new EventEmitter<{name: string, amount: number}>();
+  // 在shared里有了商品的类型定义，引用
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  onAddItem(){
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName,ingAmount);
+    // 事件传递
+    this.ingredientAdded.emit(newIngredient);
+  }
 
 }
